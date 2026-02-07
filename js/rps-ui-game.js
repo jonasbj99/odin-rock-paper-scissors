988050ded082f3858634944ade32c839705570d8
jonasbj99 <99955044+jonasbj99@users.noreply.github.com>
2026-02-07 16:03:47 +0100

    Add UI update functions

--- js/rps-ui-game.js
+++ js/rps-ui-game.js
@@ -4,18 +4,24 @@ const cWalletEl = document.querySelector("#cWalletDisp");
 const minBetEl = document.querySelector("#minBetDisp");
 const roundBetEl = document.querySelector("#roundBetDisp");
 
+/* Selected Card Placements */
 const pCardEl = document.querySelector("#pCardSelect");
 const cCardEl = document.querySelector("#cCardSelect");
 
-/* Stat Row Elements */
-const rRow = document.querySelector("#rockStat");
-const pRow = document.querySelector("#paperStat");
-const sRow = document.querySelector("#scissorsStat");
-const wRow = document.querySelector("#bigW");
-const lRow = document.querySelector("#bigL");
+/* Stat Elements */
+const pRock = document.querySelector("#pRockStat");
+const cRock = document.querySelector("#cRockStat");
+const pPaper = document.querySelector("#pPaperStat");
+const cPaper = document.querySelector("#cPaperStat");
+const pScissors = document.querySelector("#pScissorsStat");
+const cScissors = document.querySelector("#cScissorsStat");
+const pBigW = document.querySelector("#pBigW");
+const cBigW = document.querySelector("#cBigW");
+const pBigL = document.querySelector("#pBigL");
+const cBigL = document.querySelector("#cBigL");
 
 /* Starting Dialog */
-const startModal = document.querySelector("#startWindow");
+const startModal = document.querySelector("#startModal");
 const startBuyIn = document.querySelector("#buyIn");
 const startBtn = document.querySelector("#startBtn");
 startBtn.addEventListener("click", startGame);
@@ -24,15 +30,58 @@ const gameObj = {
   buyIn: 1000,
   minBet: 50,
   roundBet: 0,
-  winCount: 0,
-  lossCount: 0,
-  tieCount: 0,
+  wCount: 0,
+  lCount: 0,
+  tCount: 0,
 };
 
 const computerObj = createPlayer("ü");
 const playerObj = createPlayer("Player");
 
 startModal.showModal();
+
+/* CONSIDER CHANGING SOME OF THE START GAME FUNCTIONALITY TO WORK BASED ON SELECT ELEMENT ON CHANGE LISTENER */
+function startGame() {
+  const bI = +startBuyIn.value;
+
+  gameObj.buyIn = bI;
+  computerObj.wallet = bI;
+  playerObj.wallet = bI;
+
+  gameObj.minBet =
+    bI === 10000 ? 200 : bI === 5000 ? 150 : bI === 2500 ? 100 : 50;
+  gameObj.roundBet = gameObj.minBet;
+
+  minBetEl.textContent = gameObj.minBet;
+  updateWalletUI();
+  startModal.close();
+}
+
+function changeBet() {
+  /* Display change bet modal */
+  /*  */
+}
+
+function updateWalletUI() {
+  pWalletEl.textContent = playerObj.wallet;
+  cWalletEl.textContent = computerObj.wallet;
+}
+
+function updateStatsUI() {
+  /* Player Stats */
+  pRock.textContent = playerObj.rpsCount[0];
+  pPaper.textContent = playerObj.rpsCount[1];
+  pScissors.textContent = playerObj.rpsCount[2];
+  pBigW.textContent = playerObj.bigW;
+  pBigL.textContent = playerObj.bigL;
+
+  /* Computer Stats */
+  cRock.textContent = computerObj.rpsCount[0];
+  cPaper.textContent = computerObj.rpsCount[1];
+  cScissors.textContent = computerObj.rpsCount[2];
+  cBigW.textContent = computerObj.bigW;
+  cBigL.textContent = computerObj.bigL;
+}
 
 function createPlayer(name) {
   return {
@@ -42,43 +91,4 @@ function createPlayer(name) {
     bigW: 0,
     bigL: 0,
   };
-}
-
-function startGame() {
-  const bI = +startBuyIn.value;
-  startModal.close();
-
-  gameObj.buyIn = bI;
-  gameObj.minBet =
-    bI === 10000 ? 200 : bI === 5000 ? 150 : bI === 2500 ? 100 : 50;
-  computerObj.wallet = bI;
-  playerObj.wallet = bI;
-  updateUI();
-}
-
-function updateUI() {
-  pWalletEl.textContent = playerObj.wallet;
-  cWalletEl.textContent = computerObj.wallet;
-  minBetEl.textContent = gameObj.minBet;
-  roundBetEl.textContent = gameObj.roundBet;
-
-  /* Update Rock Stats */
-  rRow.querySelectorAll("td")[0].textContent = playerObj.rpsCount[0];
-  rRow.querySelectorAll("td")[1].textContent = computerObj.rpsCount[0];
-
-  /* Update Paper Stats */
-  pRow.querySelectorAll("td")[0].textContent = playerObj.rpsCount[1];
-  pRow.querySelectorAll("td")[1].textContent = computerObj.rpsCount[1];
-
-  /* Update Scissors Stats */
-  sRow.querySelectorAll("td")[0].textContent = playerObj.rpsCount[2];
-  sRow.querySelectorAll("td")[1].textContent = computerObj.rpsCount[2];
-
-  /* Update Biggest Win Stats */
-  wRow.querySelectorAll("td")[0].textContent = playerObj.bigW;
-  wRow.querySelectorAll("td")[1].textContent = computerObj.bigW;
-
-  /* Update Biggest Loss Stats */
-  lRow.querySelectorAll("td")[0].textContent = playerObj.bigL;
-  lRow.querySelectorAll("td")[1].textContent = computerObj.bigL;
 }
